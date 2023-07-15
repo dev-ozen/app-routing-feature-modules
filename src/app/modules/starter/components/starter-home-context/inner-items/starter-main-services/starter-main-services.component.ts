61c2726ad91a527a30679453d8f0ff61cbd66396
ozen-hub <info.ozenhub@gmail.com>
2023-07-16 01:10:19 +0530

    1

---
+++ src/app/modules/starter/components/starter-home-context/inner-items/starter-main-services/starter-main-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starter-main-services',
+  templateUrl: './starter-main-services.component.html',
+  styleUrls: ['./starter-main-services.component.scss']
+})
+export class StarterMainServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/starter/components/starter-home-context/inner-items/starter-main-slider/starter-main-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starter-main-slider',
+  templateUrl: './starter-main-slider.component.html',
+  styleUrls: ['./starter-main-slider.component.scss']
+})
+export class StarterMainSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
