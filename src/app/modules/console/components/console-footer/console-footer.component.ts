b3716ba9bd8b9ff40b2453c340615128e82a8929
ozen-hub <info.ozenhub@gmail.com>
2023-07-17 21:57:28 +0530

    2

---
+++ src/app/modules/console/components/console-footer/console-footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-console-footer',
+  templateUrl: './console-footer.component.html',
+  styleUrls: ['./console-footer.component.scss']
+})
+export class ConsoleFooterComponent implements OnInit {
+
+  date= new Date().getFullYear();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
