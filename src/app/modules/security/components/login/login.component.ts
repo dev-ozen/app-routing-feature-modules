3850c5493b67995cdbc034d13e979b16eed72916
ozen-hub <info.ozenhub@gmail.com>
2023-07-17 23:00:28 +0530

    3

---
+++ src/app/modules/security/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../../shared/service/auth.service";
+import {Title} from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+email='';
+password='';
+
+  constructor(private router: Router, private authService: AuthService, private title:Title) { }
+
+  ngOnInit(): void { //
+    this.title.setTitle('Security | login');
+    if (this.authService.isExistsToken('my-token')){
+      // verify
+      this.router.navigateByUrl('/console').then();
+    }
+  }
+
+  login(){
+    if (this.email==='h@gmail.com' && this.password==='1234'){
+      this.authService.createToken(this.email);
+      this.router.navigateByUrl('/console').then();
+    }else{
+      alert('wrong inputs')
+    }
+  }
+
+
+}
